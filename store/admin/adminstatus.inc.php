f94b9c54e2d60d97eceef23c611f0197b6044b05
Art Howard <codeguru@panlingo.com>
2018-04-18 01:12:55 -0700

    Upraded mysql to mysqli.  File is now running PHP 7.

--- store/admin/adminstatus.inc.php
+++ store/admin/adminstatus.inc.php
@@ -5,20 +5,20 @@
       echo "<table width=\"100%\" cellpadding=\"1\" border=\"1\">\n";
 
       $query = "SELECT prodid from products";
-      $result = mysql_query($query);
-      $totprods = mysql_num_rows($result);
+      $result = mysqli_query($con,$query);
+      $totprods = mysqli_num_rows($result);
 
       echo "<tr><td>Products in store</td><td>$totprods</td></tr>\n";
 
       $query="SELECT prodid from products where quantity = 0";
-      $result = mysql_query($query);
-      $totout = mysql_num_rows($result);
+      $result = mysqli_query($con,$query);
+      $totout = mysqli_num_rows($result);
 
       echo "<tr><td>Products out of stock</td><td>$totout</td></tr>\n";
 
       $query = "SELECT orderid from orders where status = 'pending'";
-      $result = mysql_query($query);
-      $totpending = mysql_num_rows($result);
+      $result = mysqli_query($con,$query);
+      $totpending = mysqli_num_rows($result);
 
       echo "<tr><td>Orders Pending</td><td>$totpending</td</tr>\n";
       echo "</table>\n";
